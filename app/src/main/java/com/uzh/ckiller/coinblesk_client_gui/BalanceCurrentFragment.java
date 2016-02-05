9ef0ba8d76f14fa0eaadf1db27d0bf3ca8b95144
Christian Killer <christian.killer@uzh.ch>
2016-02-05 15:42:48 +0100

    Adding link from Settings to Opacity of Connection Icons

--- app/src/main/java/com/uzh/ckiller/coinblesk_client_gui/BalanceCurrentFragment.java
+++ app/src/main/java/com/uzh/ckiller/coinblesk_client_gui/BalanceCurrentFragment.java
@@ -5,6 +5,7 @@ package com.uzh.ckiller.coinblesk_client_gui;
  * Created by ckiller on 12/01/16.
  */
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -19,6 +20,8 @@ import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
+import java.util.Set;
+
 /**
  * Created by ckiller on 10/01/16.
  */
@@ -26,6 +29,11 @@ import android.widget.TextView;
 public class BalanceCurrentFragment extends Fragment {
 
     private CurrencyFormatter currencyFormatter;
+    public static final String NFC_ACTIVATED = "nfc-checked";
+    public static final String BLUETOOTH_ACTIVATED = "btc-checked";
+    public static final String WIFIDIRECT_ACTIVATED = "wifi-checked";
+    public static final String CONNECTION_SETTINGS = "pref_connection_settings";
+
 
     public static BalanceCurrentFragment newInstance(int page) {
         BalanceCurrentFragment fragment = new BalanceCurrentFragment();
@@ -46,25 +54,42 @@ public class BalanceCurrentFragment extends Fragment {
         final TextView smallBalance = (TextView) view.findViewById(R.id.balance_small);
         final TextView largeBalance = (TextView) view.findViewById(R.id.balance_large);
 
-        // TODO Get the SharedPreferences on Connection Settings
-        // change android:alpha to 0.25 if not activated
-        // change android:alpha to 0.75 if activated
-
+        // TODO Move this code to the approporiate section / fragment method?
         final ImageView nfcConn = (ImageView) view.findViewById(R.id.nfc_balance);
         final ImageView bluetoothConn = (ImageView) view.findViewById(R.id.bluetooth_balance);
         final ImageView wifiConn = (ImageView) view.findViewById(R.id.wifidirect_balance);
 
-/*
+        SharedPreferences preferences = getActivity().getSharedPreferences(CONNECTION_SETTINGS,Context.MODE_PRIVATE);
+        Set<String> connectionSettings = preferences.getStringSet(CONNECTION_SETTINGS, null);
+        // defValues
+        if (connectionSettings != null) {
+            for (String s : connectionSettings) {
+                if (s.equals(NFC_ACTIVATED)) {
+                    nfcConn.setImageAlpha(100);
+                    System.out.println("[NFC]HELLO WE ARE INSIDE HERE HEHEHEH");
+                }
 
-        public static final String PREF_FILE_NAME = "PrefFile";
-        SharedPreferences preferences = getActivity().getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+                if (s.equals(BLUETOOTH_ACTIVATED)) {
+                    bluetoothConn.setImageAlpha(100);
+                    System.out.println("[BT]HELLO WE ARE INSIDE HERE HEHEHEH");
 
-*/
+                }
+                if (s.equals(WIFIDIRECT_ACTIVATED)) {
+                    wifiConn.setImageAlpha(100);
+                    System.out.println("[WIFI]HELLO WE ARE INSIDE HERE HEHEHEH");
+
+                }
+            }
+        }
+
 
         //TODO Get the actual Balance instead of dummy data
         largeBalance.setText(currencyFormatter.formatLarge("2.4431", "BTC"));
         smallBalance.setText(currencyFormatter.formatSmall("655.01", "CHF"));
 
         return view;
+
     }
 }
+
+
