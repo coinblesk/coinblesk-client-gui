1cbc2242197be968328a039e07f9093ed009fda4
Christian Killer <christian.killer@uzh.ch>
2016-02-20 15:16:14 +0100

    Customizing SendDialogFragment

--- app/src/main/java/com/uzh/ckiller/coinblesk_client_gui/ui/dialogs/SendDialogFragment.java
+++ app/src/main/java/com/uzh/ckiller/coinblesk_client_gui/ui/dialogs/SendDialogFragment.java
@@ -10,6 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TextInputLayout;
+
 
 import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -24,8 +27,16 @@ public class SendDialogFragment extends DialogFragment {
     private final static String TAG = SendDialogFragment.class.getName();
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setStyle(DialogFragment.STYLE_NO_FRAME, android.R.style.Theme_Material);
+        setStyle(DialogFragment.STYLE_NO_FRAME, android.R.style.ThemeOverlay_Material_Dark);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         this.getDialog().setTitle(R.string.send_dialog_title);
         View view = inflater.inflate(R.layout.fragment_send_dialog, container);
         view.findViewById(R.id.qr_scan_button).setOnClickListener(new View.OnClickListener() {
@@ -34,6 +45,19 @@ public class SendDialogFragment extends DialogFragment {
                 IntentIntegrator.forSupportFragment(SendDialogFragment.this).initiateScan();
             }
         });
+
+        Toolbar actionBar = (Toolbar) view.findViewById(R.id.fake_action_bar);
+        if (actionBar!=null) {
+            final SendDialogFragment window = this;
+            actionBar.setTitle("Send Bitcoins");
+            actionBar.setNavigationOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    window.dismiss();
+                }
+            });
+        }
+
+
         return view;
     }
 
