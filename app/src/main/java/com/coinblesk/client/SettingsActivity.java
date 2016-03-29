9e84621af0cbf0ae1dab8f218d86c80d008edb3c
Andreas Albrecht <albrecht@iodir.com>
2016-03-29 20:16:00 +0200

    close (restart)  wallet after network change

--- app/src/main/java/com/coinblesk/client/SettingsActivity.java
+++ app/src/main/java/com/coinblesk/client/SettingsActivity.java
@@ -1,15 +1,21 @@
 package com.coinblesk.client;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 /**
  * Created by ckiller on 17/01/16.
  */
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = SettingsActivity.class.getName();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +39,30 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_pref);
+
+            initRestartAfterNetworkChange();
+        }
+
+        private void initRestartAfterNetworkChange() {
+            Preference pref = findPreference("pref_network_list");
+            pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, final Object newValue) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle(R.string.restart)
+                            .setMessage(R.string.pref_network_changed_restart)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Log.i(TAG, "Restart wallet due to network change: " + newValue);
+                                    dialog.dismiss();
+                                    getActivity().finishAffinity();
+                                }
+                            })
+                            .create().show();
+                    return true;
+                }
+            });
         }
     }
 }
