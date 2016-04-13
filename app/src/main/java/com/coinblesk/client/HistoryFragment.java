9b25a2caf138c389a4c7c5a48e31ffc65df95b48
Andreas Albrecht <albrecht@iodir.com>
2016-04-13 12:02:45 +0200

    hint that there are no transactions yet (issue #34)

--- app/src/main/java/com/coinblesk/client/HistoryFragment.java
+++ app/src/main/java/com/coinblesk/client/HistoryFragment.java
@@ -11,11 +11,11 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.coinblesk.client.ui.widget.RecyclerView;
 import com.coinblesk.payments.Constants;
 import com.coinblesk.payments.WalletService;
 import com.coinblesk.payments.models.TransactionWrapper;
@@ -33,11 +33,14 @@ public class HistoryFragment extends android.support.v4.app.Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        recyclerView = (RecyclerView) inflater.inflate(
-                R.layout.fragment_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.txhistoryview);
+        View empty = view.findViewById(R.id.txhistory_emptyview);
+        recyclerView.setEmptyView(empty);
         recyclerView.setAdapter(new TransactionWrapperRecyclerViewAdapter(new ArrayList<TransactionWrapper>()));
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
-        return recyclerView;
+
+        return view;
     }
 
     /* ------------------- PAYMENTS INTEGRATION STARTS HERE  ------------------- */
--- app/src/main/java/com/coinblesk/client/TransactionWrapperRecyclerViewAdapter.java
+++ app/src/main/java/com/coinblesk/client/TransactionWrapperRecyclerViewAdapter.java
@@ -2,7 +2,6 @@ package com.coinblesk.client;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.coinblesk.client.helpers.UIUtils;
+import com.coinblesk.client.ui.widget.RecyclerView;
 import com.coinblesk.payments.models.TransactionWrapper;
 
 import org.ocpsoft.prettytime.PrettyTime;
